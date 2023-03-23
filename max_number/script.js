e8944403e5b8a5ef65797fdb7dd2b3b2904d27e6
Ramalingareddy1 <ramareddyc1996@gmail.com>
2023-03-23 11:51:27 +0530

    Finding max number

---
+++ max_number/script.js
@@ -0,0 +1,14 @@
+console.log('Max Number')
+
+function findMaxNumber(num1, num2){
+    return (num1>num2)? num1:num2
+}
+let checkMax1=(findMaxNumber(10,5));
+console.log('Max Number:', checkMax1);
+
+
+let checkMax2=(findMaxNumber(10,15));
+console.log('Max Number:',checkMax2 );
+
+let checkMax3=(findMaxNumber(50,50));
+console.log('Max Number:', checkMax3);
