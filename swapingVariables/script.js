787ee4b6b9a6cd309c5d20f9a328b621148b2569
Ramalingareddy1 <ramareddyc1996@gmail.com>
2023-03-23 11:25:04 +0530

    adding swaping variables

---
+++ swapingVariables/script.js
@@ -0,0 +1,15 @@
+console.log('Swaping variable');
+
+let value1='one';
+let value2='two';
+
+console.log('original', value1);
+console.log('original', value2);
+
+let value3=value1;
+value1=value2;
+value2=value3;
+
+console.log('swap', value1);
+console.log('swap', value2);
+
